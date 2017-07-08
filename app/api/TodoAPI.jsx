9e0bd8e04b831db3a18c1127565e182c25285996
bhaveshc20 <bhaveshc789@gmail.com>
2017-07-08 01:36:07 -0700

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,5 +20,30 @@ module.exports = {
       } else{
         return [];
       }
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+    // filter by show completed
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    //filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (a.completed === false && b.completed === true){
+        return -1;
+      } else if(a.completed === true && b.completed === false){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
